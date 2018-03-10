48fdc719f07a837c7cac394835514a1093e5bdcc
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-10 09:45:52 -0500

    Create InvalidArgumentException.php

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+/**
+ * Genial Framework.
+ *
+ * @link    <https://github.com/genial-framework/logger> Github repository.
+ * @license <https://github.com/genial-framework/logger/blob/master/LICENSE> GPL-3.0 License.
+ */
+
+namespace Genial\Logger\Exception;
+
+/**
+ * InvalidArgumentException.
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+}
