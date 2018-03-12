2681e1931abe2282d4e01e4ed9b96295a1682840
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-03-11 20:57:08 -0400

    Update version.php

--- boot/version.php
+++ boot/version.php
@@ -1,1 +1,12 @@
-
+<?php
+declare(strict_types=1);
+/**
+ * Genial Framework.
+ *
+ * @link    <https://github.com/genial-framework/logger> Github repository.
+ * @license <https://github.com/genial-framework/logger/blob/master/LICENSE> GPL-3.0 License.
+ */
+ 
+/* VERSION DECLARATION */
+define("GENIAL_LOGGER_VERSION_NUMBER", '1.0.0');
+define("GENIAL_LOGGER_VERSION_ID", 100000);
